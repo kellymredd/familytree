3bc31464f35b56945486337fd8700e91458e89ab
Kelly Redd <kellymredd@gmail.com>
2022-04-19 09:47:22 -0500

    Adding config after restricting api http configuration

--- src/config/config.js
+++ src/config/config.js
@@ -2,7 +2,16 @@ import firebase from "firebase/app";
 import "firebase/firestore";
 
 // need to optimize queries (only return data we need displayed, etc)
-const appConfig = null;
+// const appConfig = null;
+const appConfig = {
+  apiKey: "AIzaSyAXxthH88U-fyZCCEFq86kVR97S8A-K-c0",
+  authDomain: "roots-fs.firebaseapp.com",
+  projectId: "roots-fs",
+  storageBucket: "roots-fs.appspot.com",
+  messagingSenderId: "803644816326",
+  appId: "1:803644816326:web:22ca7fe74051500fcf50c7",
+  measurementId: "G-FYDJHVEDQN"
+};
 
 const SafeGET = Boolean(appConfig);
 
