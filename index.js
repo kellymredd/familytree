e787e1e0ff9111b96909b9c3105f131f4236f26f
Kelly Redd <kellymredd@gmail.com>
2022-04-30 20:38:55 -0500

    chore: troubleshooting api routes

--- index.js
+++ index.js
@@ -14,8 +14,8 @@ const PORT = process.env.PORT || 3000;
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
 app.use(express.static(path.join(__dirname, "build")));
 app.use(express.json({ limit: "10mb", type: "application/json" }));
-app.use(cors());
-app.use("/api", router);
+// app.use(cors());
+app.use("/api/", router);
 
 app.get("/*", function (req, res) {
   res.sendFile(path.resolve(__dirname, "./build", "index.html"));
