96fd29440cc2c54e4dc20cd0e03c1ef2bf91e1ee
Kelly Redd <kellymredd@gmail.com>
2022-04-30 21:10:02 -0500

    chore: Removing asterisk from first app.get()

--- index.js
+++ index.js
@@ -14,10 +14,9 @@ const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 app.use(express.static(path.join(__dirname, "build")));
 app.use(express.json({ limit: "10mb", type: "application/json" }));
-// app.use(cors());
 // app.use("/api", router);
 
-app.get("/*", function (req, res) {
+app.get("/", function (req, res) {
   res.sendFile(path.resolve(__dirname, "./build", "index.html"));
 });
 
