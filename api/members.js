a6b3296ee418923bd7f4200ec3dbe3e4d13fbde0
Kelly Redd <kellymredd@gmail.com>
2022-05-02 16:24:19 -0500

    chore: Fixing await error

--- api/members.js
+++ api/members.js
@@ -6,9 +6,10 @@ import mysql from "mysql2/promise";
 
 dotenv.config();
 
-const connection = await mysql.createConnection(process.env.DATABASE_URL);
+// const connection = await mysql.createConnection(process.env.DATABASE_URL);
 
 async function apis(req, res) {
+  const connection = await mysql.createConnection(process.env.DATABASE_URL);
   if (req.method === "GET") {
     const query = "SELECT * FROM member";
     const [rows] = await connection.query(query);
