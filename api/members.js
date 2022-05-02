f708b11a175770cd4dbbff37dffad6e9803a932e
Kelly Redd <kellymredd@gmail.com>
2022-05-02 16:21:31 -0500

    chore: ssl db connection errors

--- api/members.js
+++ api/members.js
@@ -1,26 +1,17 @@
 // import express from "express";
-// import dotenv from "dotenv";
-// import mysql from "mysql2/promise";
+import dotenv from "dotenv";
+import mysql from "mysql2/promise";
 // const dotenv = require("dotenv");
-import { PSDB } from "planetscale-node";
 // const mysql = require("mysql2/promise");
 
-// dotenv.config();
+dotenv.config();
 
-// const router = express.Router();
-// const connection = await mysql.createConnection(process.env.DATABASE_URL);
-
-const conn = new PSDB("familytree");
+const connection = await mysql.createConnection(process.env.DATABASE_URL);
 
 async function apis(req, res) {
-  // const connection = await mysql
-  //   .createConnection(process.env.DATABASE_URL)
-  //   .catch((err) => console.log(err));
-
   if (req.method === "GET") {
-    // return res.json([{ id: 12312312, FirstName: "Kelly" }]);
     const query = "SELECT * FROM member";
-    const [rows] = await conn.query(query);
+    const [rows] = await connection.query(query);
     res.json(rows);
   }
 }
