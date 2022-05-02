d8c71971dc4c32543dcd47b2fa042469da39c798
Kelly Redd <kellymredd@gmail.com>
2022-05-02 15:38:16 -0500

    chore: Installing psdb node package

--- api/members.js
+++ api/members.js
@@ -1,24 +1,27 @@
 // import express from "express";
 // import dotenv from "dotenv";
 // import mysql from "mysql2/promise";
-const dotenv = require("dotenv");
-const mysql = require("mysql2/promise");
+// const dotenv = require("dotenv");
+const PSDB = require("planetscale-node");
+// const mysql = require("mysql2/promise");
 
-dotenv.config();
+// dotenv.config();
 
 // const router = express.Router();
 // const connection = await mysql.createConnection(process.env.DATABASE_URL);
 
+const conn = new PSDB("familytree");
+
 async function apis(req, res) {
-  const connection = await mysql
-    .createConnection(process.env.DATABASE_URL)
-    .catch((err) => console.log(err));
+  // const connection = await mysql
+  //   .createConnection(process.env.DATABASE_URL)
+  //   .catch((err) => console.log(err));
 
   if (req.method === "GET") {
     // return res.json([{ id: 12312312, FirstName: "Kelly" }]);
     const query = "SELECT * FROM member";
-    const [rows] = await connection.query(query);
-    res.send(rows);
+    const [rows] = await conn.query(query);
+    res.json(rows);
   }
 }
 
