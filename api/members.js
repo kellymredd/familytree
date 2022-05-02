0e420be7dba301e34c4daf0c3c85cdde3833bc91
Kelly Redd <kellymredd@gmail.com>
2022-05-02 12:08:26 -0500

    chore:vAttempting to connect to db

--- api/members.js
+++ api/members.js
@@ -1,16 +1,19 @@
 // import express from "express";
 // import dotenv from "dotenv";
 // import mysql from "mysql2/promise";
-// const dotenv = require("dotenv");
-// const mysql = require("mysql2/promise");
+const dotenv = require("dotenv");
+const mysql = require("mysql2/promise");
 
-// dotenv.config();
+dotenv.config();
 
 // const router = express.Router();
-// const connection = await mysql.createConnection(process.env.DATABASE_URL);
+const connection = await mysql.createConnection(process.env.DATABASE_URL);
 
 module.exports = function (req, res) {
   if (req.method === "GET") {
-    return res.json([{ id: 12312312, FirstName: "Kelly" }]);
+    // return res.json([{ id: 12312312, FirstName: "Kelly" }]);
+    const query = "SELECT * FROM member";
+    const [rows] = await connection.query(query);
+    res.send(rows);
   }
 };
