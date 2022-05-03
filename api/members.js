e4e3e76f9b2a70ccc56a808d4eb1ca08c57cea4b
Kelly Redd <kellymredd@gmail.com>
2022-05-02 21:39:09 -0500

    chore: ssl db connection errors

--- api/members.js
+++ api/members.js
@@ -7,7 +7,9 @@ async function apis(req, res) {
   // const connection = await mysql.createConnection(
   //   `mysql://0x7slzpast8k:pscale_pw_ESx2t8hYHUwnHTfaeZx5mj4AyTXQ2_yevyplzdoM_FI@qfj0ueknrhqm.us-east-1.psdb.cloud/familytree?ssl={"rejectUnauthorized":true}`
   // );
-  const connection = await mysql.createConnection(process.env.DATABASE_URL);
+  const connection = await mysql.createConnection(
+    process.env.PLANETSCALE_PRISMA_DATABASE_URL
+  );
   if (req.method === "GET") {
     const query = "SELECT * FROM member";
     const [rows] = await connection
