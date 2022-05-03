af0a0858848640c9420ae0b917888cd721c4b674
Kelly Redd <kellymredd@gmail.com>
2022-05-02 20:45:03 -0500

    chore: ssl db connection errors

--- api/members.js
+++ api/members.js
@@ -6,11 +6,12 @@ import mysql from "mysql2/promise";
 dotenv.config();
 
 const dbUrl = process.env.DATABASE_URL;
-console.log(dbUrl);
 // const connection = await mysql.createConnection(process.env.DATABASE_URL);
 
 async function apis(req, res) {
-  const connection = await mysql.createConnection(dbUrl);
+  const connection = await mysql.createConnection(
+    `DATABASE_URL=mysql://${PLANETSCALE_DB_USERNAME}:${PLANETSCALE_DB_PASSWORD}@${PLANETSCALE_DB_HOST}/${PLANETSCALE_DB}?ssl={"rejectUnauthorized":true}`
+  );
   if (req.method === "GET") {
     const query = "SELECT * FROM member";
     const [rows] = await connection
