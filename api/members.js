22b45ab9e9fdf84c993ccec3e4631da48cf6e532
Kelly Redd <kellymredd@gmail.com>
2022-05-03 09:27:15 -0500

    chore: Aliasing column names for listing members;

--- api/members.js
+++ api/members.js
@@ -6,12 +6,13 @@ dotenv.config();
 const DB_CONN_STRING = `mysql://${process.env.PLANETSCALE_DB_USERNAME}:${process.env.PLANETSCALE_DB_PASSWORD}@${process.env.PLANETSCALE_DB_HOST}/${process.env.PLANETSCALE_DB}?ssl={"rejectUnauthorized":true}`;
 
 async function apis(req, res) {
-  // const connection = await mysql.createConnection(
-  //   `mysql://0x7slzpast8k:pscale_pw_ESx2t8hYHUwnHTfaeZx5mj4AyTXQ2_yevyplzdoM_FI@qfj0ueknrhqm.us-east-1.psdb.cloud/familytree?ssl={"rejectUnauthorized":true}`
-  // );
   const connection = await mysql.createConnection(DB_CONN_STRING);
+
+  // find a better way than IF statements
   if (req.method === "GET") {
-    const query = "SELECT * FROM member";
+    // father, first_name, gender, last_name, middle_name, mother, spouse, status, suffix, maiden_name
+    const query =
+      "SELECT first_name as firstName, middle_name as middleName, last_name as lastName, id  FROM member";
     const [rows] = await connection
       .query(query)
       .catch((error) => console.log(error));
