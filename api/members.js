67878d58c293c5dd545a7a43ffeef1adaa6d79b4
Kelly Redd <kellymredd@gmail.com>
2022-05-02 20:39:48 -0500

    chore: ssl db connection errors

--- api/members.js
+++ api/members.js
@@ -4,12 +4,13 @@ import mysql from "mysql2/promise";
 // const dotenv = require("dotenv");
 // const mysql = require("mysql2/promise");
 
-dotenv.config();
+// dotenv.config();
 
 // const connection = await mysql.createConnection(process.env.DATABASE_URL);
 
 async function apis(req, res) {
-  console.log(process.env.DATABASE_URL);
+  // console.log(process.env.DATABASE_URL);
+  dotenv.config();
   const connection = await mysql.createConnection(process.env.DATABASE_URL);
   if (req.method === "GET") {
     const query = "SELECT * FROM member";
