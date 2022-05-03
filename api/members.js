ec9a0e80a686b94f313e948cae3f0c1e0eee3dcc
Kelly Redd <kellymredd@gmail.com>
2022-05-02 21:46:27 -0500

    chore: env string

--- api/members.js
+++ api/members.js
@@ -3,13 +3,13 @@ import mysql from "mysql2/promise";
 
 dotenv.config();
 
+const DB_CONN_STRING = `mysql://${process.env.PLANETSCALE_DB_USERNAME}:${process.env.PLANETSCALE_DB_PASSWORD}@${process.env.PLANETSCALE_DB_HOST}/${process.env.PLANETSCALE_DB}?ssl={"rejectUnauthorized":true}`;
+
 async function apis(req, res) {
   // const connection = await mysql.createConnection(
   //   `mysql://0x7slzpast8k:pscale_pw_ESx2t8hYHUwnHTfaeZx5mj4AyTXQ2_yevyplzdoM_FI@qfj0ueknrhqm.us-east-1.psdb.cloud/familytree?ssl={"rejectUnauthorized":true}`
   // );
-  const connection = await mysql.createConnection(
-    process.env.PLANETSCALE_PRISMA_DATABASE_URL
-  );
+  const connection = await mysql.createConnection(DB_CONN_STRING);
   if (req.method === "GET") {
     const query = "SELECT * FROM member";
     const [rows] = await connection
