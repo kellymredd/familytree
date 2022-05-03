e72d92706f2ac9bdf8e1c0afdd6f29adcc762538
Kelly Redd <kellymredd@gmail.com>
2022-05-02 20:56:06 -0500

    chore: moved .env file into same dir

--- api/members.js
+++ api/members.js
@@ -1,17 +1,13 @@
-// import express from "express";
 import dotenv from "dotenv";
 import mysql from "mysql2/promise";
-// const dotenv = require("dotenv");
-// const mysql = require("mysql2/promise");
+
 dotenv.config();
 
-const dbUrl = process.env.DATABASE_URL;
-// const connection = await mysql.createConnection(process.env.DATABASE_URL);
-
 async function apis(req, res) {
-  const connection = await mysql.createConnection(
-    `mysql://0x7slzpast8k:pscale_pw_ESx2t8hYHUwnHTfaeZx5mj4AyTXQ2_yevyplzdoM_FI@qfj0ueknrhqm.us-east-1.psdb.cloud/familytree?ssl={"rejectUnauthorized":true}`
-  );
+  // const connection = await mysql.createConnection(
+  //   `mysql://0x7slzpast8k:pscale_pw_ESx2t8hYHUwnHTfaeZx5mj4AyTXQ2_yevyplzdoM_FI@qfj0ueknrhqm.us-east-1.psdb.cloud/familytree?ssl={"rejectUnauthorized":true}`
+  // );
+  const connection = await mysql.createConnection(process.env.DATABASE_URL);
   if (req.method === "GET") {
     const query = "SELECT * FROM member";
     const [rows] = await connection
