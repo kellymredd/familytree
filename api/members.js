d1e91a65c6c534ad6c51b24c9541532f0927d845
Kelly Redd <kellymredd@gmail.com>
2022-05-03 09:30:59 -0500

    chore: Correcting UI casing;

--- api/members.js
+++ api/members.js
@@ -12,7 +12,7 @@ async function apis(req, res) {
   if (req.method === "GET") {
     // father, first_name, gender, last_name, middle_name, mother, spouse, status, suffix, maiden_name
     const query =
-      "SELECT first_name as firstName, middle_name as middleName, last_name as lastName, id  FROM member";
+      "SELECT first_name as FirstName, middle_name as MiddleName, last_name as LastName, id  FROM member";
     const [rows] = await connection
       .query(query)
       .catch((error) => console.log(error));
