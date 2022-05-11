f4c80e1f372f945e165739f0257126593fd35be2
Kelly Redd <kellymredd@gmail.com>
2022-05-11 15:53:16 -0500

    chore: Selecting and aliasing all Profile data points

--- api/members/[mid].js
+++ api/members/[mid].js
@@ -11,7 +11,7 @@ async function member(req, res) {
   // find a better way than IF statements
   if (req.method === "GET") {
     // father, first_name, gender, last_name, middle_name, mother, spouse, status, suffix, maiden_name
-    const query = `SELECT first_name as FirstName, middle_name as MiddleName, last_name as LastName, id FROM member WHERE id = ${mid}`;
+    const query = `SELECT first_name as FirstName, middle_name as MiddleName, last_name as LastName, suffix as Suffix, date_of_birth as DOB, date_of_death as DOD, marriage_status as Status, gender as Gender, id FROM member WHERE id = ${mid}`;
     const [rows] = await connection
       .query(query)
       .catch((error) => console.log(error));
