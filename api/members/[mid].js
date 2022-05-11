7bedca69aee1c86e55de28fd667a0960a086bef0
Kelly Redd <kellymredd@gmail.com>
2022-05-11 13:58:13 -0500

    chore: Reworking api folder routes

--- api/members/[mid].js
+++ api/members/[mid].js
@@ -7,12 +7,11 @@ const DB_CONN_STRING = `mysql://${process.env.PLANETSCALE_DB_USERNAME}:${process
 
 async function member(req, res) {
   const connection = await mysql.createConnection(DB_CONN_STRING);
-
+  const { mid } = req.query;
   // find a better way than IF statements
   if (req.method === "GET") {
     // father, first_name, gender, last_name, middle_name, mother, spouse, status, suffix, maiden_name
-    const query =
-      "SELECT first_name as FirstName, middle_name as MiddleName, last_name as LastName, id FROM member WHERE id = req.params.id";
+    const query = `SELECT first_name as FirstName, middle_name as MiddleName, last_name as LastName, id FROM member WHERE id = ${mid}`;
     const [rows] = await connection
       .query(query)
       .catch((error) => console.log(error));
