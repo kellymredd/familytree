6ea28b05c3cd01f2eda7c987171e8f4c6e5e6cff
Kelly Redd <kellymredd@gmail.com>
2022-05-14 13:48:03 -0500

    chore: Starting to implement env vars for firestore config;

--- src/config/config.js
+++ src/config/config.js
@@ -4,7 +4,16 @@ import "firebase/firestore";
 // need to optimize queries (only return data we need displayed, etc)
 // const appConfig = null;
 const appConfig = {
-  apiKey: "AIzaSyAXxthH88U-fyZCCEFq86kVR97S8A-K-c0",
+  // apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  // authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  // databaseURL: process.env.NEXT_PUBLIC_FIREBASE_DATABASE_URL,
+  // projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  // storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  // messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  // appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  // measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
+
+  apiKey: process.env.REACT_PUBLIC_FIREBASE_API_KEY, //"AIzaSyAXxthH88U-fyZCCEFq86kVR97S8A-K-c0", // process.env.HG_DB_USERNAME
   authDomain: "roots-fs.firebaseapp.com",
   projectId: "roots-fs",
   storageBucket: "roots-fs.appspot.com",
