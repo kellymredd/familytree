1a9db947ebb9c62a0a23363b96be8fae1278aa67
Kelly Redd <kellymredd@gmail.com>
2022-07-15 13:35:25 -0500

    chore: Deleted procfile; Adding message so listing page doesn't blow up

--- src/list/ListMembers.js
+++ src/list/ListMembers.js
@@ -5,7 +5,7 @@ import Template from "../shared/Template";
 import useMembers from "../hooks/useMembers.hook";
 
 export default function ListMembersScreen() {
-  const [users, setUsers] = useState([]);
+  const [users, setUsers] = useState(null);
   const { getMembers } = useMembers();
 
   useEffect(() => {
@@ -21,42 +21,46 @@ export default function ListMembersScreen() {
       </Template.Head>
       <Template.Body>
         <ul className={classnames("list-group", "listUsers")}>
-          {users
-            ?.sort((a, b) => {
-              let nameA = a.FirstName.toUpperCase();
-              let nameB = b.FirstName.toUpperCase();
-              if (nameA < nameB) {
-                return -1;
-              }
-              if (nameA > nameB) {
-                return 1;
-              }
-              return 0;
-            })
-            ?.map((user, idx) => (
-              <li key={idx} className="list-group-item">
-                <Link
-                  to={`/${user.id}`}
-                  title={`View ${user.FirstName}'s profile`}
-                >
-                  {user.FirstName} {user.MiddleName} {user.LastName}
-                </Link>{" "}
-                <Link
-                  to={`/${user.id}/edit`}
-                  title={`Edit ${user.FirstName}'s profile`}
-                >
-                  <i className="fas fa-pen"></i>
-                </Link>{" "}
-                {user?.id && (
+          {users ? (
+            users
+              ?.sort((a, b) => {
+                let nameA = a.FirstName.toUpperCase();
+                let nameB = b.FirstName.toUpperCase();
+                if (nameA < nameB) {
+                  return -1;
+                }
+                if (nameA > nameB) {
+                  return 1;
+                }
+                return 0;
+              })
+              ?.map((user, idx) => (
+                <li key={idx} className="list-group-item">
                   <Link
-                    to={`/${user.id}/tree`}
-                    title={`View ${user.FirstName}'s Familiy Tree`}
+                    to={`/${user.id}`}
+                    title={`View ${user.FirstName}'s profile`}
                   >
-                    View Tree
-                  </Link>
-                )}
-              </li>
-            ))}
+                    {user.FirstName} {user.MiddleName} {user.LastName}
+                  </Link>{" "}
+                  <Link
+                    to={`/${user.id}/edit`}
+                    title={`Edit ${user.FirstName}'s profile`}
+                  >
+                    <i className="fas fa-pen"></i>
+                  </Link>{" "}
+                  {user?.id && (
+                    <Link
+                      to={`/${user.id}/tree`}
+                      title={`View ${user.FirstName}'s Familiy Tree`}
+                    >
+                      View Tree
+                    </Link>
+                  )}
+                </li>
+              ))
+          ) : (
+            <p>No Members found for your family</p>
+          )}
         </ul>
       </Template.Body>
     </Template>
