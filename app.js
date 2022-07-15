d90553638b932a3473db5e87934aa84c3cb8db73
Kelly Redd <kellymredd@gmail.com>
2022-07-15 13:15:09 -0500

    chore: Switching routing places as troubleshooting step

--- app.js
+++ app.js
@@ -2,33 +2,16 @@ var express = require("express");
 var bodyParser = require("body-parser");
 var path = require("path");
 var routes = require("./api");
-// var cors = require("cors");
 
 var PORT = process.env.PORT || 3001;
 var app = express();
 
-// allowCrossDomain = function (req, res, next) {
-//   res.header("Access-Control-Allow-Origin", "*");
-//   res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE,OPTIONS");
-//   res.header(
-//     "Access-Control-Allow-Headers",
-//     "Content-Type, Authorization, Content-Length, X-Requested-With"
-//   );
-//   if ("OPTIONS" === req.method) {
-//     res.sendStatus(200);
-//   } else {
-//     next();
-//   }
-// };
-
-// app.use(allowCrossDomain);
-
 app.use(express.static(path.join(__dirname, "dist")));
+app.use(bodyParser.json());
+app.use("/api", routes);
 app.get("/", function (req, res) {
   res.sendFile(path.join(__dirname, "dist", "index.html"));
 });
-app.use(bodyParser.json());
-app.use("/api", routes);
 
 // router.use(function (req, res, next) {
 //   res.header("X-Frame-Options", "DENY");
