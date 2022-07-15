902be37682940b0986be8be1a1ebfbf2bcef2569
Kelly Redd <kellymredd@gmail.com>
2022-07-15 13:22:56 -0500

    chore: Reverting routing order; adding procfile;

--- app.js
+++ app.js
@@ -7,11 +7,11 @@ var PORT = process.env.PORT || 3001;
 var app = express();
 
 app.use(express.static(path.join(__dirname, "dist")));
-app.use(bodyParser.json());
-app.use("/api", routes);
 app.get("/", function (req, res) {
   res.sendFile(path.join(__dirname, "dist", "index.html"));
 });
+app.use(bodyParser.json());
+app.use("/api", routes);
 
 // router.use(function (req, res, next) {
 //   res.header("X-Frame-Options", "DENY");
