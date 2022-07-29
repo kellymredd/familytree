7268cc756fb5a877d65624442e9444bae76b8d8d
Kelly Redd <kellymredd@gmail.com>
2022-07-29 10:28:10 -0500

    chore: Lil cleanup

--- api/controllers/refresh.controller.js
+++ api/controllers/refresh.controller.js
@@ -3,7 +3,6 @@ require("dotenv").config();
 const { verify } = require("jsonwebtoken");
 const { createRefreshToken } = require("../auth/auth");
 const resetRefreshToken = require("../auth/resetRefreshToken");
-// const { users } = require("../../models");
 
 const refresh = async (req, res, next) => {
   const token = req.cookies.jid;
@@ -13,21 +12,20 @@ const refresh = async (req, res, next) => {
 
   let payload = null;
   try {
-    // need jwt token (move to jwtfile maybe?)
+    // need jwt token
     payload = verify(token, process.env.REFRESH_TOKEN_SECRET);
   } catch (error) {
     console.log(error);
-    // should we be doing res.status()??
     res.status(401).json({ error: "Could not verify token." });
   }
+
+  // there is also the notion of `versioning` the token
 
   if (payload) {
     // Refresh the `refreshToken`
     resetRefreshToken(createRefreshToken(payload.username));
-
     // Refresh Users `accessToken`
     res.status(200).json({ accesToken: createRefreshToken(payload.username) });
-    // return send({ ok: true, accesToken: createRefreshToken(payload.username) });
   }
 
   // do we REALLY need to do this?
--- models/users.js
+++ models/users.js
@@ -3,7 +3,6 @@ module.exports = (sequelize, DataTypes) => {
     id: { type: DataTypes.INTEGER(11), autoIncrement: true, primaryKey: true },
     userName: { type: DataTypes.STRING, allowNull: false },
     password: { type: DataTypes.STRING, allowNull: false },
-    // refreshToken: { type: DataTypes.STRING, allowNull: false },
   });
 
   return users;
