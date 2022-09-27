c3f440278716c0e14acf6df590c3f697dcce5818
Kelly Redd <kellymredd@gmail.com>
2022-09-27 15:57:16 -0500

    chore: New eslint configuration added to package.json

--- src/shared/EventForm.js
+++ src/shared/EventForm.js
@@ -1,9 +1,9 @@
-import React from "react";
-import { Select, Button, DateInput } from "../controls/index";
-import MarriageForm from "./relationForms/MarriageForm";
-import listData from "../utils/staticLists";
+import React from 'react';
+import { Select, Button, DateInput } from '../controls/index';
+import MarriageForm from './relationForms/MarriageForm';
+import listData from '../utils/staticLists';
 
-const marriageType = ["4"];
+const marriageType = ['4'];
 
 export default function EventForm({
   event = {},
@@ -24,90 +24,88 @@ export default function EventForm({
   };
 
   return (
-    <>
-      <div className="formWrapper">
-        <h3>{event?.id ? "Edit" : "Add"} Event</h3>
-        <div className="formBody">
-          <div className="row">
-            <div className="col-4">
-              <Select
-                id="typeOfEvent"
-                label="Type of Event"
-                value={event?.typeOfEvent}
-                onChange={updateFormFields}
-                options={eventTypes}
-                selectValueKey="value"
-                selectLabelKey="label"
-              />
-            </div>
+    <div className="formWrapper">
+      <h3>{event?.id ? 'Edit' : 'Add'} Event</h3>
+      <div className="formBody">
+        <div className="row">
+          <div className="col-4">
+            <Select
+              id="typeOfEvent"
+              label="Type of Event"
+              value={event?.typeOfEvent}
+              onChange={updateFormFields}
+              options={eventTypes}
+              selectValueKey="value"
+              selectLabelKey="label"
+            />
           </div>
-          <div className="row">
-            <div className="col-4">
-              <DateInput
-                id="dateOfEvent"
-                label="Date of Event"
-                value={event.dateOfEvent}
-                onChange={updateFormFields}
-              />
-            </div>
-            <div className="col-4">
-              <Select
-                id="city"
-                label="City"
-                value={event.city}
-                options={cities}
-                selectValueKey="value"
-                selectLabelKey="label"
-                onChange={updateFormFields}
-              />
-            </div>
-            <div className="col-4">
-              <Select
-                id="county"
-                label="County"
-                value={event.county}
-                options={counties}
-                selectValueKey="value"
-                selectLabelKey="label"
-                onChange={updateFormFields}
-              />
-            </div>
-            <div className="col-4">
-              <Select
-                id="stateProvince"
-                label="State"
-                value={event.stateProvince}
-                onChange={updateFormFields}
-                options={states}
-                selectValueKey="value"
-                selectLabelKey="label"
-              />
-            </div>
-            <div className="col-4">
-              <Select
-                id="country"
-                label="Country"
-                value={event.country}
-                options={countries}
-                selectValueKey="value"
-                selectLabelKey="label"
-                onChange={updateFormFields}
-              />
-            </div>
+        </div>
+        <div className="row">
+          <div className="col-4">
+            <DateInput
+              id="dateOfEvent"
+              label="Date of Event"
+              value={event.dateOfEvent}
+              onChange={updateFormFields}
+            />
           </div>
-          {showMarriageForm ? (
-            <MarriageForm event={event} handleOnChange={setEvent} />
-          ) : null}
+          <div className="col-4">
+            <Select
+              id="city"
+              label="City"
+              value={event.city}
+              options={cities}
+              selectValueKey="value"
+              selectLabelKey="label"
+              onChange={updateFormFields}
+            />
+          </div>
+          <div className="col-4">
+            <Select
+              id="county"
+              label="County"
+              value={event.county}
+              options={counties}
+              selectValueKey="value"
+              selectLabelKey="label"
+              onChange={updateFormFields}
+            />
+          </div>
+          <div className="col-4">
+            <Select
+              id="stateProvince"
+              label="State"
+              value={event.stateProvince}
+              onChange={updateFormFields}
+              options={states}
+              selectValueKey="value"
+              selectLabelKey="label"
+            />
+          </div>
+          <div className="col-4">
+            <Select
+              id="country"
+              label="Country"
+              value={event.country}
+              options={countries}
+              selectValueKey="value"
+              selectLabelKey="label"
+              onChange={updateFormFields}
+            />
+          </div>
         </div>
-        <div className="formFooter">
-          <Button btnStyle="link" onClick={() => handleCancel()}>
-            Close
-          </Button>
-          <Button btnStyle="primary" onClick={() => handleSave(event)}>
-            Save Event
-          </Button>
-        </div>
+        {showMarriageForm ? (
+          <MarriageForm event={event} handleOnChange={setEvent} />
+        ) : null}
       </div>
-    </>
+      <div className="formFooter">
+        <Button btnStyle="link" onClick={() => handleCancel()}>
+          Close
+        </Button>
+        <Button btnStyle="primary" onClick={() => handleSave(event)}>
+          Save Event
+        </Button>
+      </div>
+    </div>
   );
 }
